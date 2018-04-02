52ffb2c49b3af5332aa1e12b197c2fb9b64460dd
SAMUEL HAILE <samishken@gmail.com>
2018-04-02 16:25:34 -0400

    hello world

--- todos/views.py
+++ todos/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .models import Todo
-
-# Create your views here.
-def index(request);
-    return HttpResponse('Hello World')
+def index(request):
+    return HttpResponse("Hello, world!")
 
 
 
